348a737d1a40e3258a9cdae7c4c2d78f8dbc50f4
abdelfetah18 <nstart0319@gmail.com>
2023-02-04 03:33:29 -0600

    new signIn/signUp ui :)

--- pages/user/sign_in.js
+++ pages/user/sign_in.js
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Loading from "../../components/Loading";
-import Input from "../../components/Input";
 import { FcGoogle } from "react-icons/fc";
 import { motion, useAnimation } from "framer-motion";
 import axios from "axios";
-import Link from "next/link";
 
-export default function SignIn(){
+export default function Signin(){
     var [username,setUsername] = useState("");
     var [password,setPassword] = useState("");
     var [waiting,setWaiting] = useState(false);
@@ -48,49 +46,40 @@ export default function SignIn(){
     }
 
     return(
-        <div className={styles.container}>
+        <div className="w-full h-screen flex flex-row items-center">
             {
                 (waiting) ? ( 
-                    <div className="flex flex-col items-center justify-center z-10 w-screen h-screen absolute top-0 left-0 bg-[#00000055]">
+                    <div className="flex flex-col items-center justify-center z-10 w-screen h-screen absolute top-0 left-0 bg-[#00000077]">
                         <Loading />
                     </div>
                  ) : ""
             }
-            <div className="w-1/3 h-full shadow-[1px_0px_20px]">
-                <div className="w-full h-full">
-                    <img className="h-full w-full object-cover" src={"https://i.pinimg.com/736x/ac/01/a7/ac01a71a22bc48266f6cd10feddb5bb5.jpg"} />
+            <div className="w-2/3 h-full bg-white flex flex-col items-center">
+                <div className="w-11/12">
+                    <img className="w-20 h-20 rounded-full bg-white" src="/logo.png" />
+                </div>
+                <div className="w-11/12 flex-grow flex flex-col items-center justify-center">
+                    <div className="text-black text-3xl font-bold">Login to Your Account</div>
+                    <div className="text-gray-500 text-base py-2">Login using social networks</div>
+                    <div className="flex flex-row items-center">
+                        <FcGoogle className="cursor-pointer text-3xl bg-gray-100 rounded-full"/>
+                    </div>
+                    <div className="text-base font-bold py-4">Or</div>
+                    <motion.div animate={alertMessageAnimation} className="opacity-0 hidden text-base text-red-600">{alertMessage}</motion.div>
+                    <div className="w-full flex flex-col items-center pb-4">
+                        <input value={username} onChange={(evt) => { setUsername(evt.target.value); }} className="w-1/2 bg-gray-100 px-3 py-2 rounded-lg my-1" placeholder="Username" type="text" />
+                        <input value={password} onKeyDown={(evt) => { if(evt.key === "Enter"){ sign_in(evt); }}} onChange={(evt) => { setPassword(evt.target.value); }} className="w-1/2 bg-gray-100 px-3 py-2 rounded-lg my-1" placeholder="Password" type="password" />
+                    </div>
+                    <div onClick={sign_in} className="cursor-pointer px-6 py-2 rounded-lg bg-gray-900 text-white font-bold">Sign in</div>
                 </div>
             </div>
-            <div className="w-2/3 h-full flex flex-col items-center p-5">
-                <div className={styles.welcom}>Welcom to my Shop</div>
-                <div className={styles.form_wrapper}>
-                    <div className={styles.form_header}>Sign in with:</div>
-                    <div className={styles.google_wrapper}>
-                        <FcGoogle />
-                        <div className={styles.google_text}>Sign in</div>
-                    </div>
-                    <div className={styles.form_header}>or with:</div>
-                    <div className={styles.form}>
-                        <motion.div animate={alertMessageAnimation} className="border-l-2 opacity-0 bg-[#efefef] hidden rounded font-semibold text-base w-2/3 my-4 py-2 px-4">{alertMessage}</motion.div>
-                        <Input type="text" value_state={[username,setUsername]} placeholder="username" />
-                        <Input type="password" value_state={[password,setPassword]} placeholder="password" />
-                    </div>
-                    <div onClick={sign_in} className={styles.btn}>Sign in</div>
-                    <Link href="/user/sign_up" ><div className={styles.s_btn}>Sign up</div></Link>
+            <div className="w-1/3 h-screen flex flex-col items-center justify-center bg-gray-900">
+                <div className="text-white text-5xl text-base font-bold" >New Here ?</div>
+                <div className="text-white text-2xl w-2/3 text-center my-4">
+                    {"Sign up and discover a great amount of new opportunities!"}
                 </div>
+                <a href="./sign_up" className="cursor-pointer px-6 py-2 rounded-lg bg-white text-black font-bold">Sign up</a>
             </div>
         </div>
     )
-}
-
-const styles = {
-    container:"w-screen h-screen flex flex-row items-center",
-    welcom:"text-2xl font-bold px-4 py-4",
-    form_wrapper:"border-2 w-5/6 h-2/3 rounded-xl flex flex-col items-center",
-    form_header:"font-semibold text-lg px-4 py-2",
-    form:"w-11/12 flex flex-col items-center py-4",
-    btn:"w-1/3 text-center px-4 py-1 text-white font-semibold cursor-pointer hover:bg-red-500 bg-blue-500 rounded-lg my-1",
-    s_btn:"w-1/3 text-center px-4 py-1 text-blue-500 font-bold cursor-pointer border-blue-500 border-2 rounded-lg my-1",
-    google_wrapper:"my-2 cursor-pointer hover:bg-gray-300 flex flex-row items-center justify-center px-4 py-2 bg-gray-200 rounded-xl",
-    google_text:"font-bold text-base ml-1"
 }
